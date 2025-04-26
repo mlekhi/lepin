f495005e17ea529708534b2ea0a974b743a10986
mlekhi <maya.lekhi1@gmail.com>
2025-04-26 12:51:46 -0400

    adding background glow for loading icon

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -17,15 +17,28 @@ export default function Loading({ className = '' }: LoadingProps) {
       className={`fixed inset-0 z-50 flex items-center justify-center bg-background transition-opacity duration-1000 ease-in-out ${className}`}
     >
       <div className="flex flex-col items-center gap-4">
-        <div className="animate-spin" style={{ animationDuration: '1s' }}>
+        <div className="relative w-16 h-16 flex items-center justify-center">
+          {/* Glow effect */}
           <Image
             src="/logo.png"
-            alt="Loading"
-            width={48}
-            height={48}
-            className="w-12 h-12 object-contain"
+            alt="Glow"
+            width={96}
+            height={96}
+            className="absolute left-1/2 top-1/2 w-24 h-24 -translate-x-1/2 -translate-y-1/2 blur-2xl opacity-80 pointer-events-none select-none"
+            aria-hidden
             priority
           />
+          {/* Spinning logo */}
+          <div className="animate-spin" style={{ animationDuration: '1s' }}>
+            <Image
+              src="/logo.png"
+              alt="Loading"
+              width={48}
+              height={48}
+              className="w-12 h-12 object-contain"
+              priority
+            />
+          </div>
         </div>
       </div>
     </div>
